a67074b2f5a4d0900442d75e3e19b7b786e6dd11
Nathaniel W. Chaney <nchaney@aos-c02q32.princeton.edu>
2015-11-11 12:24:26 -0500

    Succeded in reproducing propr

---
+++ driver.py
@@ -0,0 +1,60 @@
+import geospatialtools.gdal_tools as gdal_tools
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+#Read in the properties (top layer)
+dtype={'names': ('code','ud','ld','min','mid','max'),
+       'formats': ('S100','f4','f4','f4','f4','f4')}
+data = np.loadtxt('properties/20130606_dlr_reference_and_uncertainty_propr.txt',
+     skiprows=1,dtype=dtype,usecols=(0,1,2,5,6,7),delimiter=',')
+mask = data['ud'] == 0.0
+tmp = {}
+for var in data.dtype.names:
+ tmp[var] = data[var][mask]
+data = tmp
+data['id'] = np.arange(len(data['code']))
+nc = len(data['code'])
+nd = 1000
+metadata = gdal_tools.retrieve_metadata('probabilities/%s.tif' % data['code'][0])
+probabilities = np.zeros((nc,metadata['ny'],metadata['nx']))
+for ic in xrange(nc):
+ file = 'probabilities/%s.tif' % data['code'][ic]
+ probabilities[ic,:,:] = gdal_tools.read_raster(file)[:]
+#Sort the array
+argsort = np.flipud(np.argsort(probabilities,axis=0))
+probs = np.flipud(np.sort(probabilities,axis=0))
+#Draw n draws per soil class
+draws = np.zeros((nc,nd))
+for ic in data['id']:
+ #c = 0.1
+ loc = data['min'][ic]
+ scale = data['max'][ic]-data['min'][ic]
+ mode = (data['mid'][ic] - data['min'][ic])/(data['max'][ic] - data['min'][ic])
+ tmp = stats.triang.rvs(mode,loc=loc,scale=scale,size=nd)
+ draws[ic,:] = tmp
+#Create the draws array
+output = np.zeros((nd,metadata['ny'],metadata['nx']))
+for i in xrange(output.shape[1]):
+ print i
+ for j in xrange(output.shape[2]):
+  samples = np.ceil(nd*probs[:,i,j])
+  mask = samples > 0
+  if np.sum(samples) > nd:
+   dif = np.sum(samples) - nd
+   for k in xrange(samples.size):
+    samples[k] = samples[k] - 1
+    dif = dif - 1
+    if dif == 0:break
+  args = argsort[:,i,j]
+  tmp = 0
+  for k in xrange(np.sum(mask)):
+   output[tmp:tmp+samples[k],i,j] = draws[args[k],0:samples[k]]
+   tmp = tmp + samples[k]
+print np.sum(output == 0.0)
+output = np.sort(output,axis=0)
+
+#Construct the distributions
+plt.imshow(np.mean(output,axis=0))
+plt.colorbar()
+plt.show()
