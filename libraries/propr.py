a2ef416dc0bac0d3958a8edf476908ca08d2faf8
Nathaniel Chaney <Nathaniel.Chaney@gaea11.ncrc.gov>
2016-11-03 20:16:45 -0400

    stable place

--- libraries/propr.py
+++ libraries/propr.py
@@ -6,8 +6,9 @@ import scipy.stats as stats
 import time
 import warnings
 warnings.filterwarnings("ignore")
+undef = -9999.0
 
-def initialize_netcdf_output(instance):
+def initialize_netcdf_output(instance,nx,ny):
 
  ud = instance.fp_properties.variables['upper_depth'][:]
  ld = instance.fp_properties.variables['lower_depth'][:]
@@ -37,21 +38,25 @@ def initialize_netcdf_output(instance):
   #Create new variables
   shape = instance.variables[var]['shape']
   if len(shape) == 2:
-   grp.createVariable('mean','f4',('z','y','x'))
-   grp.createVariable('max','f4',('z','y','x'))
-   grp.createVariable('min','f4',('z','y','x'))
-   grp.createVariable('alpha','f4',('z','y','x'))
-   grp.createVariable('beta','f4',('z','y','x'))
+   grp.createVariable('mean','f4',('z','y','x'),chunksizes=(1,ny,nx))
+   grp.createVariable('max','f4',('z','y','x'),chunksizes=(1,ny,nx))
+   grp.createVariable('min','f4',('z','y','x'),chunksizes=(1,ny,nx))
+   grp.createVariable('alpha','f4',('z','y','x'),chunksizes=(1,ny,nx))
+   grp.createVariable('beta','f4',('z','y','x'),chunksizes=(1,ny,nx))
   else:
-   grp.createVariable('mean','f4',('y','x'))
-   grp.createVariable('max','f4',('y','x'))
-   grp.createVariable('min','f4',('y','x'))
-   grp.createVariable('alpha','f4',('y','x'))
-   grp.createVariable('beta','f4',('y','x'))
+   grp.createVariable('mean','f4',('y','x'),chunksizes=(ny,nx))
+   grp.createVariable('max','f4',('y','x'),chunksizes=(ny,nx))
+   grp.createVariable('min','f4',('y','x'),chunksizes=(ny,nx))
+   grp.createVariable('alpha','f4',('y','x'),chunksizes=(ny,nx))
+   grp.createVariable('beta','f4',('y','x'),chunksizes=(ny,nx))
+  #Initialize to undef
+  stats = ['mean','min','max','alpha','beta']
+  #for stat in stats:
+  # grp[stat][:] = undef
 
  return fp
 
-def calculate_properties_on_each_block(instance,fp,ix,iy):
+def calculate_properties_on_each_block(instance,fp,ix,iy,iblock):
 
  #Define the arrays
  mr = instance.metadata['maxrank']
@@ -59,22 +64,33 @@ def calculate_properties_on_each_block(instance,fp,ix,iy):
  instance.prob = instance.metadata["vm"]["prob_scalar_multiplier"]*instance.prob
  instance.rank = instance.fp_probabilities.variables[instance.metadata['vm']['rank']][0:mr,iy,ix]
  instance.block_ids = np.ma.getdata(np.unique(instance.rank))
+ #instance.array = np.zeros((instance.metadata['nd'],instance.prob.shape[1],instance.prob.shape[2]),dtype='f4', order='F')
+ 
+ #If there are no unique ids then return
+ bids = instance.block_ids
+ bids = bids[bids >= 0]
+ if len(np.unique(bids)) == 0:
+  for var in instance.metadata['vars']:
+   if len(instance.variables[var]['shape']) == 2:
+    for il in xrange(instance.nl):
+     for stat in ['mean','min','max','alpha','beta']:
+       fp[var][stat][il,iy,ix] = undef
+   else:
+     for stat in ['mean','min','max','alpha','beta']:
+       fp[var][stat][iy,ix] = undef
+  return
 
  #Curate the probabilities (always needs to sum to 100)
  instance.prob = instance.prob/np.sum(instance.prob,axis=0)
  
  #Create the input data for a variable for each layer
- #vars = instance.fp_properties.groups.keys()
  vars = instance.metadata['vars']
  for var in vars:
 
   t0 = time.time()
-  print "Calculating %s maps" % var
+  #print instance.metadata['process_id'],"Calculating %s maps" % var
   #Retrieve the variables
   grp = fp[var]
-  #vpc5 = fp.variables['%s_pc5' % var]
-  #vpc95 = fp.variables['%s_pc95' % var]
-  #vmean = fp.variables['%s_mean' % var]
  
   if len(instance.variables[var]['shape']) == 2:
 
@@ -96,9 +112,6 @@ def calculate_properties_on_each_block(instance,fp,ix,iy):
     #Output the properties
     for stat in output:
      fp[var][stat][il,iy,ix] = output[stat]
-    #vpc5[il,iy,ix] = output['pc5']
-    #vpc95[il,iy,ix] = output['pc95']
-    #vmean[il,iy,ix] = output['mean']
 
   else:
 
@@ -117,12 +130,10 @@ def calculate_properties_on_each_block(instance,fp,ix,iy):
     #Output the properties
     for stat in output:
      fp[var][stat][iy,ix] = output[stat]
-    #vpc5[iy,ix] = output['pc5']
-    #vpc95[iy,ix] = output['pc95']
-    #vmean[iy,ix] = output['mean']
-  
-  print time.time() - t0
 
+  t1 = time.time()
+  print instance.metadata['process_id'],"Finished %s maps in %f seconds" % (var,(t1-t0))
+  
  return
 
 def run(file):
@@ -130,14 +141,16 @@ def run(file):
  #Initialize the propr class
  instance = initialize(file)
 
- #Initialize the output file
- print "Initializing output file"
- fp = initialize_netcdf_output(instance)
-
- #Iterate per block
+ #Get the block info
  n = instance.metadata['nblocks']+1
  xbounds = np.ceil(np.linspace(instance.ix[0],instance.ix[-1],n)).astype(np.int32)
  ybounds = np.ceil(np.linspace(instance.iy[0],instance.iy[-1],n)).astype(np.int32)
+
+ #Initialize the output file
+ print instance.metadata['process_id'],"Initializing output file"
+ nx = xbounds[1] - xbounds[0]
+ ny = ybounds[1] - ybounds[0]
+ fp = initialize_netcdf_output(instance,nx,ny)
 
  iblock = 0
  for i in xrange(len(xbounds)-1):
@@ -145,9 +158,9 @@ def run(file):
   for j in xrange(len(ybounds)-1):
    iy = np.arange(xbounds[j],ybounds[j+1]+1)
    iblock = iblock + 1
-   print "Working on block %d/%d" % (iblock,(n-1)*(n-1))
+   print instance.metadata['process_id'],"Working on block %d/%d" % (iblock,(n-1)*(n-1))
    #Calculate the properties for this block
-   calculate_properties_on_each_block(instance,fp,ix,iy)
+   calculate_properties_on_each_block(instance,fp,ix,iy,iblock)
 
  #Finalize netcdf file
  print "Finalizing output file"
@@ -201,47 +214,27 @@ class initialize:
   draws = np.zeros((nc,nd)).astype(np.float32)
   draws[:] = undef
   #Construct mapping array
-  mapping = np.zeros(np.max(data['id'])+1).astype(np.int32)
+  #mapping = np.zeros(np.max(data['id'])+1).astype(np.int32)
+  mapping = np.zeros(np.max(ids)+1).astype(np.int32)
   mapping[ids] = np.arange(nc)
   #Compute the index in the data
   idx = np.in1d(mids,ids)
   #Determine the classes that have information
   m = idx & (data['mean'] != undef)
   #Draw from beta distribution
-  tmp = np.random.beta(data['alpha'][m],data['beta'][m],(nd,np.sum(m)))
+  #tmp = np.random.beta(data['alpha'][m],data['beta'][m],(nd,np.sum(m))) THIS CREATES BOUNDARIES
+  tmp = []
+  for i in xrange(np.sum(m)):
+   #Define the random seed
+   rnd = np.random.RandomState(self.metadata['seed'])
+   tmp.append(rnd.beta(data['alpha'][m][i],data['beta'][m][i],(nd,)))
+  tmp = np.array(tmp).T
   #Rescale using min and max
   tmp = tmp*(data['max'][m] - data['min'][m]) + data['min'][m]
   #Place the samples in the draws array
   m = np.in1d(ids,mids[m])
   draws[m,:] = tmp.T
   
-  #m = data['mean']
-  '''ic = 0
-  print time.time() - tic
-  for id in ids:
-   #Find index of id
-   if id in mids:
-    idx = mids.index(id)
-    if data['mean'][idx] == -9999:
-     draws[ic,:] = -9999
-     mapping[id] = ic
-    else:
-     alpha = data['alpha'][idx]
-     beta = data['beta'][idx]
-     min = data['min'][idx]
-     max = data['max'][idx]
-     #draw from beta distribution
-     tmp = np.random.beta(alpha,beta,(nd,))
-     #rescale using min and max
-     tmp = tmp*(max - min) + min
-     draws[ic,:] = tmp
-     mapping[id] = ic
-   else:
-    draws[ic,:] = -9999
-    mapping[id] = ic
-   ic += 1
-  print time.time() - tic'''
-
   return (draws,mapping)
 
  #Calculate the mapped properties
@@ -250,36 +243,28 @@ class initialize:
   #Create the array of draws
   t0 = time.time()
   (draws,mapping) = self.draw_from_distribution(data)
-  print 'sampling',time.time() - t0
+  #print 'sampling',time.time() - t0
 
   #Create the array of draws
   t0 = time.time()
   ncmax = self.metadata['ncmax']
+  #array = self.array
   array = propr_tools_fortran.assign_draws(self.prob,self.rank,draws,mapping,ncmax)
-  print 'placing data',time.time() - t0
+  #print 'placing data',time.time() - t0
 
   #Compute the beta parameters
   t0 = time.time()
   min = np.min(array,axis=0)
   max = np.max(array,axis=0)
   mean = np.mean(array,axis=0)
-  nmean = np.mean((array - min[np.newaxis,:,:])/(max[np.newaxis,:,:]-min[np.newaxis,:,:]),axis=(0,))
-  nvar = np.var((array - min[np.newaxis,:,:])/(max[np.newaxis,:,:]-min[np.newaxis,:,:]),axis=(0,))
+  #narray = (array - min[np.newaxis,:,:])/(max[np.newaxis,:,:]-min[np.newaxis,:,:])
+  narray = (array - min)/(max-min)
+  nmean = np.mean(narray,axis=(0,))
+  nvar = np.var(narray,axis=(0,))
   alpha = ((1-nmean)/nvar - (1/nmean))*nmean**2
   beta = alpha*(1/nmean - 1)
-  print 'creating beta parameters',time.time() - t0
+  #print 'creating beta parameters',time.time() - t0
   
-  #Sort the data
-  #array = np.sort(array,axis=0)
-
-  #Compute the 5th pct, 95th pct, and mean
-  #ipc5 = np.percentile(np.arange(self.metadata['nd']),5,interpolation='nearest')
-  #ipc95 = np.percentile(np.arange(self.metadata['nd']),95,interpolation='nearest')
-  #output = {}
-  #output['mean'] = np.mean(array,axis=0)
-  #output['pc5'] = array[ipc5,:,:]
-  #output['pc95'] = array[ipc95,:,:]
-
   #Assemble output
   output = {}
   output['mean'] = mean
